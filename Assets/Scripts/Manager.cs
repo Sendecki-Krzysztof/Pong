4b39f4081f7de3e2aeada934cacafc194b02a2e2
Sendecki-Krzysztof <sendecki.krzysztof19@gmail.com>
2021-03-29 00:44:27 -0500

    Created Pause added hot keys
    Created a new pause Method to stop the game as well as made a few hotkeys for easier use of the  buttons commands

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -7,6 +7,7 @@ public class Manager : MonoBehaviour
     public static int PlayerScore1 = 0;
     public static int PlayerScore2 = 0;
     public int winningScore = 10;
+    private bool ispaused = false;
 
     public GUISkin layout;
 
@@ -35,26 +36,79 @@ public class Manager : MonoBehaviour
         GUI.Label(new Rect(Screen.width / 2 - 150 - 12, 20, 100, 100), "" + PlayerScore1);
         GUI.Label(new Rect(Screen.width / 2 + 150 + 12, 20, 100, 100), "" + PlayerScore2);
 
-        if (GUI.Button(new Rect(Screen.width / 2 - 60, 35, 100, 53), "RESTART"))
-        {
-            PlayerScore1 = 0;
-            PlayerScore2 = 0;
-            theBall.SendMessage("resetGame", 0.5f, SendMessageOptions.RequireReceiver);
+        buttonHandler();
+        checkScore();
+    }
+
+    // Method to handle the placement and initialization of new GUI buttons on the game board. 
+    void buttonHandler() {
+        if (GUI.Button(new Rect(Screen.width / 2 - 70, 10, 100, 40), "RESTART")) {
+            handleRestart();
         }
-        if (GUI.Button(new Rect(Screen.width / 2 + 40, 35, 60, 53), "QUIT")) {
+
+        if (GUI.Button(new Rect(Screen.width / 2 + 31, 10, 60, 40), "QUIT")) {
             quit();
         }
 
-        if (PlayerScore1 == 10)
+        if (GUI.Button(new Rect(Screen.width / 2 - 70, 51, 161, 30), "PAUSE")) {
+            handlePauseGame();
+        }
+
+    }
+
+    // Checks if either of the players have reached the winning score.
+    void checkScore() {
+
+        if (PlayerScore1 == winningScore)
         {
             GUI.Label(new Rect(Screen.width / 2 - 150, 200, 2000, 1000), "PLAYER ONE WINS");
             theBall.SendMessage("resetBall", null, SendMessageOptions.RequireReceiver);
         }
-        else if (PlayerScore2 == 10)
+        else if (PlayerScore2 == winningScore)
         {
             GUI.Label(new Rect(Screen.width / 2 - 150, 200, 2000, 1000), "PLAYER TWO WINS");
             theBall.SendMessage("resetBall", null, SendMessageOptions.RequireReceiver);
         }
+    }
+
+
+    // If the pause button or key have been pressed, checks if game is paused. If it is then it will be unpaused, if not 
+    // then it will be paused.
+    void handlePauseGame() {
+        if (!ispaused)
+        {
+            Time.timeScale = 0;
+            ispaused = true;
+        }
+        else if (ispaused)
+        {
+            Time.timeScale = 1;
+            ispaused = false;
+        }
+    }
+
+    // Handles the restarting of the game if the hotkey or button are pressed
+    void handleRestart() {
+        PlayerScore1 = 0;
+        PlayerScore2 = 0;
+        theBall.SendMessage("resetGame", 0.5f, SendMessageOptions.RequireReceiver);
+    }
+
+    // Checks if any hot keys have been pressed.
+    void hotKeys() {
+        if (Input.GetKey(KeyCode.R))
+        {
+            handleRestart();
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            handlePauseGame();
+        }
+
+        if (Input.GetKey(KeyCode.Escape)){
+            quit();
+        } 
     }
 
 
@@ -68,6 +122,7 @@ public class Manager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        hotKeys();
     }
+        
 }
